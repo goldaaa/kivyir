34fdd50e3e14235437871b00398809e0904ebd0b
navid <109505057+goldaaa@users.noreply.github.com>
2022-10-26 01:23:11 +0330

    Update __init__.py

--- kivyir/FaCleaning/__init__.py
+++ kivyir/FaCleaning/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.0.0"
-__programer__ = "navid nasiri"
+__version__ = "0.1.0"
+__author__ = "navid nasiri"
 __github__ = "https://github.com/goldaaa"
 __gmail__ = "goldaaa.program@gmail.com"
 
