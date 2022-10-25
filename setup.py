7afcff35102f6cb955c07de36ba87cc8e89d0cf8
navid <109505057+goldaaa@users.noreply.github.com>
2022-10-26 01:24:37 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # coding=utf-8
 
 from setuptools import setup
-import os
 import io
 
 with io.open("README.md", "r", encoding="utf-8") as fh:
@@ -13,15 +12,11 @@ setup(
     description="Improving the display of Persian text for Persian speakers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='1.0.0',
+    version='0.1.0',
     platforms="ALL",
     license="MIT",
-    packages=['kivyir'],
-    install_requires=['configparser; python_version <"3"', 'future'],
-    extras_require={
-        'with-fonttools': ['fonttools>=4.0; python_version >="3"',
-                           'fonttools>=3.0,<4.0; python_version <"3"']
-    },
+    packages=['kivyir', 'test'],
+    install_requires=["kivy>=2.0.0", "pillow"],
     author="navid nasiri",
     author_email="goldaaa.program@gmail.com",
     maintainer="navid nasiri",
@@ -32,15 +27,23 @@ setup(
     keywords="kivy persian farsi iran",
     url="https://github.com/goldaaa/kivyir",
     download_url="https://github.com/goldaaa/kivyir/tarball/master",
+    package_data={
+        "kivyir": ["font/*.ttf",]
+    },
+    setup_requires=[],
+    python_requires=">=3.5",
     classifiers=[
-        "Natural Language :: Persian",
-        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: Persian',
+        "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        'Topic :: Software Development :: User Interfaces',
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
+    ],  # https://test.pypi.org/pypi?%3Aaction=list_classifiers
 )
