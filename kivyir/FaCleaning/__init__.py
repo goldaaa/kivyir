a79857e0401c49a7bea50af414374bdb6ef5dc18
navid <109505057+goldaaa@users.noreply.github.com>
2022-10-28 02:37:05 +0330

    Delete kivyir/FaCleaning directory

--- kivyir/FaCleaning/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.1.0"
-__author__ = "navid nasiri"
-__github__ = "https://github.com/goldaaa"
-__gmail__ = "goldaaa.program@gmail.com"
-
-
-from .Data import *
-from .Cleaning import *
-from .LanguageCheck import *
