ae265eaeca8198de08ee22acbc2ab0497df96fd2
navid <109505057+goldaaa@users.noreply.github.com>
2022-10-28 02:39:37 +0330

    Update __init__.py

--- kivyir/__init__.py
+++ kivyir/__init__.py
@@ -1,20 +1,9 @@
-__version__ = "0.1.0"
 __author__ = "navid nasiri"
 __github__ = "https://github.com/goldaaa"
 __gmail__ = "goldaaa.program@gmail.com"
 
-from .ConfigText import *
-from .ConfigInput import *
+__all__ = ('Config', 'ConfigBase', 'IrLabel', 'IrTextInput')
 
-from .FaCleaning import *
-
-from .IrTextInput import *
+from .ConfigBase import *
 from .IrLabel import *
-
-
-from kivy import *
-try:
-    from kivymd import *
-except ImportError:
-    pass
-
+from .IrTextInput import *
